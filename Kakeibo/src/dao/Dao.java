e05b2dff323f4caccdbabbece775524ac23d1424
Kiyoshi Kumagai <baervalley.key@gmail.com>
2018-08-20 17:01:28 +0900

    searchAllメソッドの作成

--- Kakeibo/src/dao/Dao.java
+++ Kakeibo/src/dao/Dao.java
@@ -3,12 +3,87 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dto.Dto;
 
 public class Dao {
 
 	private String content;
-	private int year, month, day, judgment, money;
+	private int year;
+	private int month;
+	private int day;
+	private int judgment;
+	private int money;
+	public static ArrayList<Dto> searchAll(){
+		ArrayList<Dto> list = new ArrayList<Dto>();
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:65534/shushi?useSSL=false",
+					"root",
+					"12345");
+
+			String sql = "SELECT * FROM shushi";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+
+			while(rs.next() == true ){
+				int id = rs.getInt("id");
+				String content = rs.getString("content");
+				int year = rs.getInt("yesr");
+				int month = rs.getInt("month");
+				int day = rs.getInt("day");
+				int judgment = rs.getInt("judgment");
+				int money = rs.getInt("money");
+				list.add(new Dto(content, id, year, month, day, judgment, money));
+			}
+		}
+		catch (ClassNotFoundException e) {
+				System.out.println("JDBCドライバが見つかりません。");
+				e.printStackTrace();
+		} catch (SQLException e){
+			System.out.println("DBアクセスに失敗しました。");
+			e.printStackTrace();
+		} finally {
+			try {
+				if( rs != null){
+					rs.close();
+				}
+			} catch(SQLException e){
+				System.out.println("DB切断時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if( pstmt != null){
+					pstmt.close();
+				}
+			} catch(SQLException e){
+				System.out.println("DB切断時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+
+			try {
+				if( con != null){
+					con.close();
+				}
+			} catch (SQLException e){
+				System.out.println("DB切断時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+		}
+
+
+		return list;
+	}
 
 	public void insert(String content, int year, int month, int day, int judgment, int money) {
 		Connection con = null;
@@ -18,7 +93,7 @@ public class Dao {
 			Class.forName("com.mysql.jdbc.Driver");
 
 			con = DriverManager.getConnection(
-					"jdbc:mysql://localhost:65534/library?useSSL=false",
+					"jdbc:mysql://localhost:65534/shushi?useSSL=false",
 					"root",
 					"12345");
 
